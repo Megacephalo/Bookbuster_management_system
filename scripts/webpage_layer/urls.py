55026da6d73ef5e60a093fc0313b6ddb4f5c9282
Charly Huang <charly.charlongo@gmail.com>
2019-12-06 02:44:12 -0500

    Ported the LMS tutorial from https://github.com/mdn/django-locallibrary-tutorial

--- scripts/webpage_layer/urls.py
+++ scripts/webpage_layer/urls.py
@@ -14,8 +14,35 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf.urls import include, url
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('library_app/', include('library_app.urls')),
+]
+
+
+
+
+# Use static() to add url mapping to serve static files during development (only)
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+urlpatterns+= static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+
+#Add URL maps to redirect the base URL to our application
+from django.views.generic import RedirectView
+urlpatterns += [
+    path('', RedirectView.as_view(url='/library_app/', permanent=True)),
+]
+
+
+
+#Add Django site authentication urls (for login, logout, password management)
+urlpatterns += [
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
